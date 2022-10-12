bbed7235cb4dde01494a5380bee148ca37d9ee8b
Hticedurak <hticedurak@gmail.com>
2022-10-12 12:20:19 +0300

    react

--- config/constants.js
+++ config/constants.js
@@ -6,6 +6,19 @@ export const colors = {
     grey: '#647c73',
 }
 
+import { Alert } from "react-native";
+
+export const twoButtonAlert = () => {
+    Alert.alert(
+        "Warning",
+        "You are about to delete this product. Are you sure?",
+        [
+            { text: "Yes", onPress: () => console.log("OK Deleted") },
+            { text: "No", onPress: () => console.log("No Deleted") }
+        ]
+    )
+}
+
 export const id = {
 
 }
